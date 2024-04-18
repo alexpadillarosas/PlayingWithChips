d14d2cca1025cf5dec9a05871774759ac56c2734
curly lemon peel <alexpadillarosas@gmail.com>
2024-04-18 18:30:14 +1000

    clean up

--- app/src/main/java/com/blueradix/android/playingwithchips/MainActivity.java
+++ app/src/main/java/com/blueradix/android/playingwithchips/MainActivity.java
@@ -1,25 +1,7 @@
 package com.blueradix.android.playingwithchips;
 
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.blueradix.android.playingwithchips.databinding.ActivityMainBinding;
-import com.blueradix.android.playingwithchips.dragons.Dragon;
-import com.blueradix.android.playingwithchips.dragons.Sign;
-import com.google.android.material.chip.Chip;
-import com.google.android.material.snackbar.Snackbar;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 //http://www.schoolofdragons.com/how-to-train-your-dragon/screenshots-gallery/dragon-pictures
-    
+
 
 }
